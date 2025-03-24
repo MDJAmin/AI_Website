0d3b92276a60e3f26e0ea0c55a82cf361b226b77
MDJAmin <166019288+MDJAmin@users.noreply.github.com>
2025-03-24 12:10:51 +0430

    Feat: Navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,10 +5,26 @@ export default function NavBar() {
     <nav className="fixed top-0 left-0 w-full bg-transparent z-50">
       <div className="mx-auto flex justify-center items-center h-16">
         <ul className="flex space-x-6 gap-20 font-bold text-2xl">
-          <li><a href="#info" className="text-gray-500 hover:text-black transition-colors">Information</a></li>
-          <li><a href="#Slider" className="text-gray-500 hover:text-black transition-colors">Slider</a></li>
-          <li><a href="#Contact" className="text-gray-500 hover:text-black transition-colors">Contact</a></li>
-          <li><a href="#Footer" className="text-gray-500 hover:text-black transition-colors">Footer</a></li>
+          <li>
+            <a href="#info" className="text-gray-500 hover:text-black transition-colors border-b-2 border-transparent hover:border-black">
+              Information
+            </a>
+          </li>
+          <li>
+            <a href="#Slider" className="text-gray-500 hover:text-black transition-colors border-b-2 border-transparent hover:border-black">
+              Slider
+            </a>
+          </li>
+          <li>
+            <a href="#Contact" className="text-gray-500 hover:text-black transition-colors border-b-2 border-transparent hover:border-black">
+              Contact
+            </a>
+          </li>
+          <li>
+            <a href="#Footer" className="text-gray-500 hover:text-black transition-colors border-b-2 border-transparent hover:border-black">
+              Footer
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
